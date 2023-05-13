1f82a49efe1dd40c62f3a54e0608bcc90b2259ba
Matthew Chen <Matthew@shorttimer.com>
2023-05-13 12:13:04 -0700

    Changed naming, added initial command

---
+++ src/main/java/net/fabricmc/Acapella/Acapella.java
@@ -0,0 +1,39 @@
+package net.fabricmc.Acapella;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.minecraft.server.command.CommandManager.*;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.text.Text;
+
+public class Acapella implements ModInitializer {
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    //public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Beat Mincecraft");
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("beatMinecraft")
+         .executes(context -> {
+             // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+             context.getSource().sendMessage(Text.literal("Beating Minecraft..."));
+             return 1;
+
+        })));
+    }
+
+}
